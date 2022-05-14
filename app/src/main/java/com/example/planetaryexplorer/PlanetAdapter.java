9b642db38c9b8e4efcba79bbfe02e3d25c5852ab
b21mihpa <b21mihpa@student.his.se>
2022-05-14 22:11:56 +0200

    added: Glide for downloading image data

--- app/src/main/java/com/example/planetaryexplorer/PlanetAdapter.java
+++ app/src/main/java/com/example/planetaryexplorer/PlanetAdapter.java
@@ -1,17 +1,23 @@
 package com.example.planetaryexplorer;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetViewHolder> {
     private List<Planet> planets;
+
+    private Context context;
 
     public PlanetAdapter(List<Planet> planets) {
         this.planets = planets;
@@ -25,6 +31,7 @@ public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetView
 
     @Override
     public void onBindViewHolder(@NonNull PlanetAdapter.PlanetViewHolder holder, int position) {
+        Glide.with(context).load(planets.get(position).imageUrl).into(holder.planetImage);
         holder.planetName.setText(planets.get(position).name);
         holder.planetAge.setText("Age: " + planets.get(position).age + " billion years");
         holder.planetRadius.setText("Radius: " + planets.get(position).radius + " kilometers");
@@ -36,13 +43,17 @@ public class PlanetAdapter extends RecyclerView.Adapter<PlanetAdapter.PlanetView
     }
 
     public class PlanetViewHolder extends RecyclerView.ViewHolder {
-        private TextView planetName;
-        private TextView planetAge;
-        private TextView planetRadius;
+        private final ImageView planetImage;
+        private final TextView planetName;
+        private final TextView planetAge;
+        private final TextView planetRadius;
 
         public PlanetViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            context = itemView.getContext();
+
+            planetImage = itemView.findViewById(R.id.planet_image);
             planetName = itemView.findViewById(R.id.planet_name);
             planetAge = itemView.findViewById(R.id.planet_age);
             planetRadius = itemView.findViewById(R.id.planet_radius);
