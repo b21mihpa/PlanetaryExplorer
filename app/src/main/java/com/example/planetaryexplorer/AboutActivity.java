c2f7b8c7bf4d2400a7f89cfba4584e5d5e68d440
b21mihpa <b21mihpa@student.his.se>
2022-05-16 20:24:00 +0200

    updated: about screen

--- app/src/main/java/com/example/planetaryexplorer/AboutActivity.java
+++ app/src/main/java/com/example/planetaryexplorer/AboutActivity.java
@@ -3,8 +3,11 @@ package com.example.planetaryexplorer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -12,6 +15,9 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        final ImageView milkyWay = findViewById(R.id.milky_way);
+        Glide.with(this).load("https://upload.wikimedia.org/wikipedia/commons/8/82/Milky_Way_Galaxy.jpg").into(milkyWay);
 
         final Button backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(view -> startActivity(new Intent(this, MainActivity.class)));
